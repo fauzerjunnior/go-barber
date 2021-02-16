18f9446491b46d7a852ab7e7954e8e4196354888
Fauzer Junior <fauzerjunnior@hotmail.com>
2021-02-16 01:33:43 -0300

    Add login screen

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -5,12 +5,26 @@ import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
 
       <Title>Faça o seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          // eslint-disable-next-line no-console
+          console.log('click');
+        }}>
+        Entrar
+      </Button>
     </Container>
   );
 };
