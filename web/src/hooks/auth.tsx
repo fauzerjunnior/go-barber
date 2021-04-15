bf8049a9b9d08be68d4a9bc84cba6d908a047e7e
Fauzer Junior <fauzerjunnior@hotmail.com>
2021-04-14 22:04:24 -0300

    Add header

--- web/src/hooks/auth.tsx
+++ web/src/hooks/auth.tsx
@@ -1,9 +1,15 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -12,7 +18,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   // eslint-disable-next-line no-unused-vars
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
